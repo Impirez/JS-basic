5ec3573c284b41dcd91e105f194ed75a37d5d4f0
Cesar Espinoza <Ceazoffi@gmail.com>
2022-12-29 17:40:16 -0500

    añadimos hoisting

---
+++ hoisting.js
@@ -0,0 +1,28 @@
+var Noombre //Declarar
+Noombre = "cesar"//inicializar
+
+
+
+    //Hoisting con variables
+
+        console.log(nombre)
+
+        var nombre = "cesar"
+        
+    
+    //Hoisting con Funciones
+
+        hey();
+
+        function hey() {
+            console.log("HOLA " + key)
+        }
+
+        var key = "nombre"
+
+
+
+
+
+
+        
